7a1429932b949e20372adc630930296d8ec68caa
pawelmajcher <51097162+pawelmajcher@users.noreply.github.com>
2025-12-02 19:33:24 +0100

    Day 02

---
+++ Sources/Day02.swift
@@ -0,0 +1,52 @@
+import Algorithms
+
+struct Day02: AdventDay {
+  var data: String
+
+  var ranges: [ClosedRange<Int>] {
+    data
+      .trimmingCharacters(in: .whitespacesAndNewlines)
+      .split(separator: ",").map {
+        let boundEnds = $0.split(separator: "-")
+        let boundMin: Int = Int(boundEnds[0])!
+        let boundMax: Int = Int(boundEnds[1])!
+        
+        return boundMin...boundMax
+      }
+  }
+  
+  var entities: [[Int]] {
+    data.split(separator: "\n\n").map {
+      $0.split(separator: "\n").compactMap { Int($0) }
+    }
+  }
+
+  func part1() -> Int {
+    let invalidNumbers = ranges.flatMap { range in
+      range.filter { number in
+        let numberString = String(number)
+        guard numberString.count.isMultiple(of: 2) else { return false }
+        
+        let firstHalf = numberString.prefix(numberString.count / 2)
+        let secondHalf = numberString.suffix(numberString.count / 2)
+        
+        return firstHalf == secondHalf
+      }
+    }
+    
+    return invalidNumbers.reduce(0, +)
+  }
+
+  func part2() -> Int {
+    let invalidNumbers = ranges.flatMap { range in
+      range.filter { number in
+        let numberString = String(number)
+        let repeatingRegex = /([0-9]+)\1+/
+        
+        return ((try! repeatingRegex.wholeMatch(in: numberString)) != nil)
+      }
+    }
+    
+    return invalidNumbers.reduce(0, +)
+  }
+}
